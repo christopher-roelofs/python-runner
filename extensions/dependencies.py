7833d241639a9d4dcd8a9af8ae10ae22519387d8
Croelofs <christopher.roelofs@gmail.com>
2025-07-08 14:43:15 -0400

    Update dependencies.py

--- extensions/dependencies.py
+++ extensions/dependencies.py
@@ -1,17 +1,3 @@
 import requests
 import selenium
 from .selenium_support import patched_webdriver as webdriver, patched_options as Options
-
-def get_exports():
-    return {
-        "requests": requests,
-        "selenium": selenium,
-        "webdriver": webdriver,
-        "Options": Options,
-    }
-
-def list_dependencies():
-    return {
-        "requests": requests.__version__,
-        "selenium": selenium.__version__,
-    }
